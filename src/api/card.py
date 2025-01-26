3c149e5216ede4e76ec4ee5b418b0abca6289364
ruro24 <rumukoro@hotmail.com>
2025-01-26 23:53:18 +0000

    basic api and react app created

--- src/api/card.py
+++ src/api/card.py
@@ -12,5 +12,12 @@ def get_deck():
     return data  # Return deck as JSON
 
 
+@app.route("/ai/", methods=["POST"])
+def get_deck():
+    with open("flashCardData.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
 if __name__ == "__main__":
     app.run(debug=True)
